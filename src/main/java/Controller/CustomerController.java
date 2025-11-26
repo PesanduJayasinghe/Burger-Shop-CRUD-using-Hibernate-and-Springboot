ed42118192d600bbb46a94e6d851d3b226fb8633
PesanduJayasinghe <pesanduse@gmail.com>
2025-11-26 13:27:42 +0530

    Created CustomerController class and fully implemented

---
+++ src/main/java/Controller/CustomerController.java
@@ -0,0 +1,49 @@
+package example.Controller;
+
+import example.Model.DTO.CustomerDto;
+import example.Model.Entity.Customer;
+import example.Service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    @GetMapping
+    public String sample(){
+        return "Customer ready";
+    }
+
+    @PostMapping("/add")
+    public void addCustomer(@RequestBody CustomerDto customerDto){
+        customerService.addCustomer(customerDto);
+    }
+
+    @PutMapping("/update")
+    public void updateCustomer(@RequestBody CustomerDto customerDto){
+        customerService.updateCustomer(customerDto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteCustomer(@PathVariable String id){
+        customerService.deleteCustomer(id);
+    }
+
+    @GetMapping("/all")
+    public List<Customer> getAllCustomer(){
+        return customerService.getAllCustomer();
+    }
+
+    @GetMapping("search/{id}")
+    public Customer searchCustomer(@PathVariable String id){
+        return customerService.searchCustomer(id);
+    }
+
+
+}
