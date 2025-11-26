1419b97aa5983d45883b60315ca628b05723b977
PesanduJayasinghe <pesanduse@gmail.com>
2025-11-26 15:24:54 +0530

    Created Item entity class file

---
+++ src/main/java/Model/Entity/Item.java
@@ -0,0 +1,23 @@
+package Model.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+@Entity
+public class Item {
+
+    @Id
+    private String ItemCode;
+    private String Description;
+    private String PackSize;
+    private String UnitPrice;
+    private String QtyOnHand;
+
+}
